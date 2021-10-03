fad61e0c4948f0f9244df2f2526e0c366b3aceaa
Joseph <joseph.larracas098@gmail.com>
2021-10-03 19:03:53 +0800

    Update

---
+++ app/src/main/java/com/exam/country_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.exam.country_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Log", "pushs");
+    }
+}
